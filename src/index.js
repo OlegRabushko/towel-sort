620ff87a6af863875d850af9d903fa8c7e2e7728
OlegRabushko <72011766+OlegRabushko@users.noreply.github.com>
2021-02-10 15:35:10 +0300

    fix: fix task

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 //You should implement your task here.
 module.exports = function towelSort(matrix) {
+    if (!matrix || matrix.length < 1) {
+        return [];
+    }
     let a = [];
     for (let i = 0; i < matrix.length; i++) {
         if (i % 2 != 0) {
